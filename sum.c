8cd6377ff93db0afdc58d21801fa82a65b4b06c9
Karthik481 <55242391+Karthik481@users.noreply.github.com>
2020-07-01 14:05:21 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+main()
+{
+    int a,b,c,sum;
+    printf("Enter three numbers");
+    scanf("%d%d%d",&a,&b,&c);
+    sum=a+b+c;
+    printf("\nSUM OF %d+%d+%d IS %d",a,b,c,sum);
+}
